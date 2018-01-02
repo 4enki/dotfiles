a2b1a7dffb2a38ad274be9ba354cbabdf03c3966
Sergey Glazov <yaglazov@gmail.com>
2018-01-02 20:19:57 +0400

    :hatching_chick:

---
+++ osx/Install.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+# install brew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew update
+brew upgrade
+
+# install apps
+
+brew install tree
+brew install git
+brew install wget
+
+brew tap caskroom/cask
+brew tap caskroom/versions
+
+function installcask() {
+  brew cask install "${@}" 2> /dev/null
+}
+
+# random stuff
+installcask skype
+installcask telegram-desktop
+installcask slack
+
+# browsers
+installcask google-chrome
+installcask opera
+installcask firefox
+installcask firefoxdeveloperedition
+
+# editors
+installcask sublime-text
+installcask atom
+installcask visual-studio-code
+
+# devtools
+installcask iterm2
+installcask virtualbox
+installcask filezilla
+installcask zeplin
